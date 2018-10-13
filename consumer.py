1816ea20ecef435d3687c10fd72d6f51e2d4ad43
Kevin Liu <kevin@potatofrom.space>
2018-10-13 18:10:48 -0400

    Add some api boi

---
+++ consumer.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+cluster_spec = {"ps": ["localhost:65062"], "worker": []}
+#ps = tf.train.Server(cluster, job_name="ps", task_index=0)
+
+
+@app.route("/api/get_cluster_spec")
+def get_cluster_spec():
+    return str(cluster_spec)
+
+
+@app.route("/api/ready")
+def is_ready():
+    return str(False)
+
+
+@app.route("/api/worker/add/<host>", methods=["POST"])
+def add_worker(host: str):
+    cluster_spec['worker'].append(host)
+    return get_cluster_spec()
+
+
+@app.route("/api/worker/del/<host>", methods=["POST"])
+def del_worker(host: str):
+    cluster_spec['worker'].remove(host)
+    return get_cluster_spec()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9001)
