880c69adf093e9a13dc9fb5ab7553f0952f63aa1
Kevin Liu <kevin@potatofrom.space>
2018-10-13 15:23:57 -0400

    Add generic files

---
+++ default.nix
@@ -0,0 +1,23 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "env";
+
+  # Mandatory boilerplate for buildable env
+  env = buildEnv { name = name; paths = buildInputs; };
+  builder = builtins.toFile "builder.sh" ''
+    source $stdenv/setup; ln -s $env $out
+  '';
+
+  # Customizable development requirements
+  buildInputs = [
+    # Add packages from nix-env -qaP | grep -i needle queries
+    python3
+    python3Packages.tensorflow
+  ];
+
+  # Customizable development shell setup with at last SSL certs set
+  shellHook = ''
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -isystem ${python3}/include/python3.6"
+    source venv/bin/activate
+  '';
+}
